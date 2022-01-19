1510ceef840f4f834de78eeb6773b04690f3f385
fabuloso <dalfovo.alessandro@gmail.com>
2022-01-19 16:30:47 +0100

    asde

--- dist/actions.js
+++ dist/actions.js
@@ -104,7 +104,7 @@ var createNewFileAction = function () {
                     case 1:
                         workDir = _b.sent();
                         resultDir = (0, path_1.resolve)(workDir, 'result');
-                        ctx.logger.info(JSON.stringify(ctx.input.values));
+                        ctx.logger.info(JSON.stringify(ctx.input));
                         ecto = ctx.input.values.ecto;
                         flags = ['--no-install'];
                         if (ecto === false) {
